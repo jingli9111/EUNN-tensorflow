7261dcc467ef251f40c373e68822fac6bd94266a
Ivan Ivanov <ivan@vankata.tk>
2017-07-12 16:34:20 -0400

    Odd hidden_size support

--- EUNN_Ivan.py
+++ EUNN_Ivan.py
@@ -26,56 +26,76 @@ def EUNN_param(hidden_size, capacity=2, FFT=False, comp=False):
     
     theta_phi_initializer = init_ops.random_uniform_initializer(-np.pi, np.pi)
     
-    sizeB = capacity//2
-    sizeA = capacity - sizeB
+    capacityB = capacity//2
+    capacityA = capacity - capacityB
 
-    params_theta_0 = vs.get_variable("theta_0", [sizeA, int(hidden_size/2)], initializer=theta_phi_initializer)
-    cos_theta_0 = array_ops.reshape(math_ops.cos(params_theta_0),[sizeA,-1,1])
-    sin_theta_0 = array_ops.reshape(math_ops.sin(params_theta_0),[sizeA,-1,1])
+    hidden_sizeA = hidden_size//2
+    hidden_sizeB = (hidden_size-1)//2
+    
+    params_theta_0 = vs.get_variable("theta_0", [capacityA, hidden_sizeA], initializer=theta_phi_initializer)
+    cos_theta_0 = array_ops.reshape(math_ops.cos(params_theta_0),[capacityA,-1,1])
+    sin_theta_0 = array_ops.reshape(math_ops.sin(params_theta_0),[capacityA,-1,1])
  
-    params_theta_1 = vs.get_variable("theta_1", [sizeB, int(hidden_size/2)-1], initializer=theta_phi_initializer)
-    cos_theta_1 = array_ops.reshape(math_ops.cos(params_theta_1),[sizeB,-1,1])
-    sin_theta_1 = array_ops.reshape(math_ops.sin(params_theta_1),[sizeB,-1,1])
+    params_theta_1 = vs.get_variable("theta_1", [capacityB, hidden_sizeB], initializer=theta_phi_initializer)
+    cos_theta_1 = array_ops.reshape(math_ops.cos(params_theta_1),[capacityB,-1,1])
+    sin_theta_1 = array_ops.reshape(math_ops.sin(params_theta_1),[capacityB,-1,1])
 
     if comp:
-        params_phi_0 = vs.get_variable("phi_0", [sizeA, int(hidden_size/2)], initializer=theta_phi_initializer)
-        cos_phi_0 = array_ops.reshape(math_ops.cos(params_phi_0),[sizeA,-1,1])
-        sin_phi_0 = array_ops.reshape(math_ops.sin(params_phi_0),[sizeA,-1,1])
+        params_phi_0 = vs.get_variable("phi_0", [capacityA, hidden_sizeA], initializer=theta_phi_initializer)
+        cos_phi_0 = array_ops.reshape(math_ops.cos(params_phi_0),[capacityA,-1,1])
+        sin_phi_0 = array_ops.reshape(math_ops.sin(params_phi_0),[capacityA,-1,1])
 
-        cos_list_0_re = array_ops.reshape(array_ops.concat([cos_theta_0, math_ops.multiply(cos_theta_0, cos_phi_0)], 2),[sizeA,-1])
-        cos_list_0_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(cos_theta_0), math_ops.multiply(cos_theta_0, sin_phi_0)], 2),[sizeA,-1])
+        cos_list_0_re = array_ops.reshape(array_ops.concat([cos_theta_0, math_ops.multiply(cos_theta_0, cos_phi_0)], 2),[capacityA,-1])
+        cos_list_0_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(cos_theta_0), math_ops.multiply(cos_theta_0, sin_phi_0)], 2),[capacityA,-1])
+        if hidden_sizeA*2 != hidden_size:
+            cos_list_0_re = array_ops.concat([cos_list_0_re,tf.ones([capacityA,1])],1)
+            cos_list_0_im = array_ops.concat([cos_list_0_im,tf.zeros([capacityA,1])],1)
         cos_list_0 = math_ops.complex(cos_list_0_re, cos_list_0_im)
         
-        sin_list_0_re = array_ops.reshape(array_ops.concat([sin_theta_0, -math_ops.multiply(sin_theta_0, cos_phi_0)], 2),[sizeA,-1])
-        sin_list_0_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(sin_theta_0), -math_ops.multiply(sin_theta_0, sin_phi_0)], 2),[sizeA,-1])
+        sin_list_0_re = array_ops.reshape(array_ops.concat([sin_theta_0, -math_ops.multiply(sin_theta_0, cos_phi_0)], 2),[capacityA,-1])
+        sin_list_0_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(sin_theta_0), -math_ops.multiply(sin_theta_0, sin_phi_0)], 2),[capacityA,-1])
+        if hidden_sizeA*2 != hidden_size:
+            sin_list_0_re = array_ops.concat([sin_list_0_re,tf.zeros([capacityA,1])],1)
+            sin_list_0_im = array_ops.concat([sin_list_0_im,tf.zeros([capacityA,1])],1)
         sin_list_0 = math_ops.complex(sin_list_0_re, sin_list_0_im)
 
-        params_phi_1 = vs.get_variable("phi_1", [sizeB, int(hidden_size/2)-1], initializer=theta_phi_initializer)
-        cos_phi_1 = array_ops.reshape(math_ops.cos(params_phi_1),[sizeB,-1,1])
-        sin_phi_1 = array_ops.reshape(math_ops.sin(params_phi_1),[sizeB,-1,1])
+        params_phi_1 = vs.get_variable("phi_1", [capacityB, hidden_sizeB], initializer=theta_phi_initializer)
+        cos_phi_1 = array_ops.reshape(math_ops.cos(params_phi_1),[capacityB,-1,1])
+        sin_phi_1 = array_ops.reshape(math_ops.sin(params_phi_1),[capacityB,-1,1])
 
-        cos_list_1_re = array_ops.reshape(array_ops.concat([cos_theta_1, math_ops.multiply(cos_theta_1, cos_phi_1)], 2),[sizeB,-1])
-        cos_list_1_re = array_ops.concat([tf.ones((sizeB,1)), cos_list_1_re, tf.ones((sizeB,1))], 1)
-        cos_list_1_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(cos_theta_1), math_ops.multiply(cos_theta_1, sin_phi_1)], 2),[sizeB,-1])
-        cos_list_1_im = array_ops.concat([tf.zeros((sizeB,1)), cos_list_1_im, tf.zeros((sizeB,1))], 1)
+        cos_list_1_re = array_ops.reshape(array_ops.concat([cos_theta_1, math_ops.multiply(cos_theta_1, cos_phi_1)], 2),[capacityB,-1])
+        cos_list_1_re = array_ops.concat([tf.ones((capacityB,1)), cos_list_1_re], 1)
+        cos_list_1_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(cos_theta_1), math_ops.multiply(cos_theta_1, sin_phi_1)], 2),[capacityB,-1])
+        cos_list_1_im = array_ops.concat([tf.zeros((capacityB,1)), cos_list_1_im], 1)
+        if hidden_sizeB*2 != hidden_size-1:
+            cos_list_1_re = array_ops.concat([cos_list_1_re,tf.ones([capacityB,1])],1)
+            cos_list_1_im = array_ops.concat([cos_list_1_im,tf.zeros([capacityB,1])],1)
         cos_list_1 = math_ops.complex(cos_list_1_re, cos_list_1_im)
         
-        sin_list_1_re = array_ops.reshape(array_ops.concat([sin_theta_1, -math_ops.multiply(sin_theta_1, cos_phi_1)], 2),[sizeB,-1])
-        sin_list_1_re = array_ops.concat([tf.zeros((sizeB,1)), sin_list_1_re, tf.zeros((sizeB,1))], 1)
-        sin_list_1_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(sin_theta_1), -math_ops.multiply(sin_theta_1, sin_phi_1)], 2),[sizeB,-1])
-        sin_list_1_im = array_ops.concat([tf.zeros((sizeB,1)), sin_list_1_im, tf.zeros((sizeB,1))], 1)
+        sin_list_1_re = array_ops.reshape(array_ops.concat([sin_theta_1, -math_ops.multiply(sin_theta_1, cos_phi_1)], 2),[capacityB,-1])
+        sin_list_1_re = array_ops.concat([tf.zeros((capacityB,1)), sin_list_1_re], 1)
+        sin_list_1_im = array_ops.reshape(array_ops.concat([array_ops.zeros_like(sin_theta_1), -math_ops.multiply(sin_theta_1, sin_phi_1)], 2),[capacityB,-1])
+        sin_list_1_im = array_ops.concat([tf.zeros((capacityB,1)), sin_list_1_im], 1)
+        if hidden_sizeB*2 != hidden_size-1:
+            sin_list_1_re = array_ops.concat([sin_list_1_re,tf.zeros([capacityB,1])],1)
+            sin_list_1_im = array_ops.concat([sin_list_1_im,tf.zeros([capacityB,1])],1)
         sin_list_1 = math_ops.complex(sin_list_1_re, sin_list_1_im)
     else:
-        cos_list_0 = array_ops.reshape(array_ops.concat([cos_theta_0, cos_theta_0], 2),[sizeA,-1])
-        sin_list_0 = array_ops.reshape(array_ops.concat([sin_theta_0, -sin_theta_0], 2),[sizeA,-1])
+        cos_list_0 = array_ops.reshape(array_ops.concat([cos_theta_0, cos_theta_0], 2),[capacityA,-1])
+        sin_list_0 = array_ops.reshape(array_ops.concat([sin_theta_0, -sin_theta_0], 2),[capacityA,-1])
+        if hidden_sizeA*2 != hidden_size:
+            cos_list_0 = array_ops.concat([cos_list_0,tf.ones([capacityA,1])],1)
+            sin_list_0 = array_ops.concat([sin_list_0,tf.zeros([capacityA,1])],1)
         
-        cos_list_1 = array_ops.reshape(array_ops.concat([cos_theta_1, cos_theta_1],2),[sizeB,-1])
-        cos_list_1 = array_ops.concat([tf.ones((sizeB,1)),cos_list_1,tf.ones((sizeB,1))],1)
-        
-        sin_list_1 = array_ops.reshape(array_ops.concat([sin_theta_1, -sin_theta_1],2),[sizeB,-1])
-        sin_list_1 = array_ops.concat([tf.zeros((sizeB,1)),sin_list_1,tf.zeros((sizeB,1))],1);
+        cos_list_1 = array_ops.reshape(array_ops.concat([cos_theta_1, cos_theta_1],2),[capacityB,-1])
+        cos_list_1 = array_ops.concat([tf.ones((capacityB,1)),cos_list_1],1)
+        sin_list_1 = array_ops.reshape(array_ops.concat([sin_theta_1, -sin_theta_1],2),[capacityB,-1])
+        sin_list_1 = array_ops.concat([tf.zeros((capacityB,1)),sin_list_1],1)
+        if hidden_sizeB*2 != hidden_size-1:
+            cos_list_1 = array_ops.concat([cos_list_1,tf.zeros([capacityB,1])],1)
+            sin_list_1 = array_ops.concat([sin_list_1,tf.zeros([capacityB,1])],1)
     
-    if sizeB != sizeA:
+    if capacityB != capacityA:
         if comp:
             cos_list_1 = array_ops.concat([cos_list_1,math_ops.complex(tf.zeros([1,hidden_size]),tf.zeros([1,hidden_size]))],0)
             sin_list_1 = array_ops.concat([sin_list_1,math_ops.complex(tf.zeros([1,hidden_size]),tf.zeros([1,hidden_size]))],0)
@@ -83,10 +103,10 @@ def EUNN_param(hidden_size, capacity=2, FFT=False, comp=False):
             cos_list_1 = array_ops.concat([cos_list_1,tf.zeros([1,hidden_size])],0)
             sin_list_1 = array_ops.concat([sin_list_1,tf.zeros([1,hidden_size])],0)
 
-    v1 = tf.reshape(tf.concat([cos_list_0, cos_list_1], 1), [sizeA*2, hidden_size])
-    v2 = tf.reshape(tf.concat([sin_list_0, sin_list_1], 1), [sizeA*2, hidden_size])
+    v1 = tf.reshape(tf.concat([cos_list_0, cos_list_1], 1), [capacityA*2, hidden_size])
+    v2 = tf.reshape(tf.concat([sin_list_0, sin_list_1], 1), [capacityA*2, hidden_size])
 
-    if sizeB != sizeA:
+    if capacityB != capacityA:
         v1 = tf.slice(v1,[0,0],[capacity,hidden_size])
         v2 = tf.slice(v2,[0,0],[capacity,hidden_size])
 
@@ -114,16 +134,44 @@ def EUNN_loop(h, L, v1_list, v2_list, D):
      
         def evenI(off):
             s = int(off.get_shape()[1])
-            off = array_ops.reshape(off,[-1,s//2,2])
-            off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
+
+            def evenS(off,s):
+                off = array_ops.reshape(off,[-1,s//2,2])
+                off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
+                return off
+
+            def oddS(off,s):
+                off, helper = array_ops.split(off,[s-1,1],1)
+                s-=1
+                off = array_ops.reshape(off,[-1,s//2,2])
+                off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
+                off = array_ops.concat([off,helper],1)
+                return off
+
+            off = control_flow_ops.cond(gen_math_ops.equal(gen_math_ops.mod(s,2),0), lambda: evenS(off,s), lambda: oddS(off,s))
             return off
 
         def oddI(off):
-            s = int(off.get_shape()[1]) - 2
-            helper1, off, helper2 = array_ops.split(off,[1,s,1],1)
-            off = array_ops.reshape(off,[-1,s//2,2])
-            off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
-            off = array_ops.concat([helper1, off, helper2],1)
+            s = int(off.get_shape()[1])
+            
+            helper, off = array_ops.split(off,[1,s-1],1)
+            s-=1
+
+            def evenS(off,s):
+                off, helper = array_ops.split(off,[s-1,1],1)
+                s-=1
+                off = array_ops.reshape(off,[-1,s//2,2])
+                off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
+                off = array_ops.concat([off,helper],1)
+                return off
+
+            def oddS(off,s):
+                off = array_ops.reshape(off,[-1,s//2,2])
+                off = array_ops.reshape(array_ops.reverse(off,[2]),[-1,s])
+                return off
+
+            off = control_flow_ops.cond(gen_math_ops.equal(gen_math_ops.mod(s,2),0), lambda: oddS(off,s), lambda: evenS(off,s))
+            off = array_ops.concat([helper, off],1)
             return off
 
         off = control_flow_ops.cond(gen_math_ops.equal(gen_math_ops.mod(i,2),0), lambda: evenI(off), lambda: oddI(off))
--- demo.sh
+++ demo.sh
@@ -1,1 +1,1 @@
-python copying_task.py EURNN -T 100 -I 200 -F False -C True -B 128 -H 128 -L 8
+python copying_task.py EURNN -T 100 -I 200 -F False -C True -B 128 -H 129 -L 7
